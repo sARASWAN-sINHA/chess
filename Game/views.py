8bc1e368d38258af7abd59c3585feb3bb1b1367f
Saraswan Sinha <saraswan1999@gmail.com>
2023-07-24 01:27:11 +0530

    Re-factoring the views for readability

--- Game/views.py
+++ Game/views.py
@@ -10,45 +10,45 @@ class PawnPositionsViewSet(ViewSet):
     @action(detail=False, methods=['GET'])
     def knight(self, request, *args, **kwargs):
         '''
-            Will generate the valid position for Knight
+            Will generate all the valid positions for Knight
             given the positions of other pawns.
         '''
         positions = request.data
-        queen_all_positions, bishop_all_positions, knight_all_positions, rook_all_positions = generate_all_positions_for_pawns(positions=positions)
-        knight_valid_position_chess_coordinates = find_valid_position_for_pawn('knight', queen_all_positions, bishop_all_positions, knight_all_positions, rook_all_positions)
+        pawns_all_positions = generate_all_positions_for_pawns(positions=positions)
+        knight_valid_position_chess_coordinates = find_valid_position_for_pawn('knight', *pawns_all_positions)
         return Response({'valid_response': knight_valid_position_chess_coordinates}, status=HTTP_200_OK)
     
     @action(detail=False, methods=['GET'])
     def rook(self, request, *args, **kwargs):
         '''
-            Will generate the valid position for Rook
+            Will generate all the valid positions for Rook
             given the positions of other pawns.
         '''
         positions = request.data
-        queen_all_positions, bishop_all_positions, knight_all_positions, rook_all_positions = generate_all_positions_for_pawns(positions=positions)
-        rook_valid_position_chess_coordinates = find_valid_position_for_pawn('rook', queen_all_positions, bishop_all_positions, knight_all_positions, rook_all_positions)
+        pawns_all_positions = generate_all_positions_for_pawns(positions=positions)
+        rook_valid_position_chess_coordinates = find_valid_position_for_pawn('rook', *pawns_all_positions)
         return Response({'valid_response': rook_valid_position_chess_coordinates}, status=HTTP_200_OK)
     
     @action(detail=False, methods=['GET'])
     def bishop(self, request, *args, **kwargs):
         '''
-            Will generate the valid position for Bishop
+            Will generate all the valid positions for Bishop
             given the positions of other pawns.
         '''
         positions = request.data
-        queen_all_positions, bishop_all_positions, knight_all_positions, rook_all_positions = generate_all_positions_for_pawns(positions=positions)
-        bishop_valid_position_chess_coordinates = find_valid_position_for_pawn('bishop', queen_all_positions, bishop_all_positions, knight_all_positions, rook_all_positions)
+        pawns_all_positions = generate_all_positions_for_pawns(positions=positions)
+        bishop_valid_position_chess_coordinates = find_valid_position_for_pawn('bishop', *pawns_all_positions)
         return Response({'valid_response': bishop_valid_position_chess_coordinates}, status=HTTP_200_OK)
     
     @action(detail=False, methods=['GET'])
     def queen(self, request, *args, **kwargs):
         '''
-            Will generate the valid position for Queen
+            Will generate the all valid positions for Queen
             given the positions of other pawns.
         '''
         positions = request.data
-        queen_all_positions, bishop_all_positions, knight_all_positions, rook_all_positions = generate_all_positions_for_pawns(positions=positions)
-        queen_valid_position_chess_coordinates = find_valid_position_for_pawn('queen', queen_all_positions, bishop_all_positions, knight_all_positions, rook_all_positions)
+        pawns_all_positions = generate_all_positions_for_pawns(positions=positions)
+        queen_valid_position_chess_coordinates = find_valid_position_for_pawn('queen', *pawns_all_positions)
         return Response({'valid_response': queen_valid_position_chess_coordinates}, status=HTTP_200_OK)
     
 
